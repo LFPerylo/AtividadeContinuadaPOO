1cc655aaacdcafc32873e4fb24c45ac03890ebc7
Luis Felipe Perylo Benicio <142416610+LFPerylo@users.noreply.github.com>
2024-09-25 14:56:17 -0300

    commit inicial

---
+++ src/br/com/cesarschool/poo/titulos/entidades/EntidadeOperadora.java
@@ -0,0 +1,82 @@
+package br.com.cesarschool.poo.titulos.entidades;
+
+/*
+ * Esta classe deve ter os seguintes atributos:
+ * identificador, do tipo long
+ * nome, do tipo String
+ * autorizadoAcao, do tipo double
+ * saldoAcao, do tipo double
+ * saldoTituloDivida, do tipo double
+ *
+ * Deve ter um construtor p�blico que inicializa os atributos identificador, nome
+ * e autorizadoAcao. Deve ter m�todos set/get p�blicos para os atributos identificador, nome
+ * e autorizadoAcao. O atributo identificador � read-only fora da classe.
+ *
+ * Os atributos saldoAcao e saldoTituloDivida devem ter apenas m�todos get p�blicos.
+ *
+ * Outros m�todos p�blicos:
+ *
+ *  void creditarSaldoAcao(double valor): deve adicionar valor ao saldoAcao
+ *  void debitarSaldoAcao(double valor): deve diminuir valor de saldoAcao
+ *  void creditarSaldoTituloDivida(double valor): deve adicionar valor ao saldoTituloDivida
+ *  void debitarSaldoTituloDivida(double valor): deve diminuir valor de saldoTituloDivida
+ */
+public class EntidadeOperadora {
+    private long identificador;
+    private String nome;
+    private double autorizadoAcao;
+    private double saldoAcao;
+    private double saldoTituloDivida;
+
+    public void debitarSaldoTituloDivida(double valor) {
+        saldoTituloDivida -= valor;
+    }
+
+    public void creditarSaldoTituloDivida(double valor) {
+        saldoTituloDivida += valor;
+    }
+
+    public void debitarSaldoAcao(double valor) {
+        saldoAcao -= valor;
+    }
+
+    public void creditarSaldoAcao(double valor) {
+        saldoAcao += valor;
+    }
+
+    public EntidadeOperadora(long identificador, String nome, double autorizadoAcao) {
+        super();
+        this.identificador = identificador;
+        this.nome = nome;
+        this.autorizadoAcao = autorizadoAcao;
+    }
+
+    public double getSaldoAcao() {
+        return saldoAcao;
+    }
+
+    public double getSaldoTituloDivida() {
+        return saldoTituloDivida;
+    }
+
+    public long getIdentificador() {
+        return identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getAutorizadoAcao() {
+        return autorizadoAcao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAutorizadoAcao(double autorizadoAcao) {
+        this.autorizadoAcao = autorizadoAcao;
+    }
+
+}
